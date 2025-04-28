f172afbe00df68f0f38e86364a29eeb350cd39c4
Noel Prado <noelfpb@Noels-MacBook-Pro.local>
2025-04-28 15:13:50 +0900

    fixed

--- Gates/graph_generator.py
+++ Gates/graph_generator.py
@@ -8,7 +8,7 @@ import pandas as pd
 data = pd.DataFrame({
     'Input A (V)': [0.10, 0.10, 4.90, 4.90],
     'Input B (V)': [0.10, 4.90, 0.10, 4.90],
-    'Output': [0.9803, 1.08, 0.79, 4.29]
+    'Output': [2, 5.09, 5.07, 5.08]
 })
 
 # Create labels for each combination with A on top of B
@@ -22,12 +22,12 @@ data['Label'] = [
 # Plot bar chart
 plt.figure(figsize=(6, 5), dpi= 180)
 plt.bar(data['Label'], data['Output'], color='skyblue')
-plt.title('AND Gate', fontsize = 16)
+plt.title('OR Gate', fontsize = 16)
 plt.xlabel('Input Combinations',fontsize = 16)
 plt.ylabel('Output (V)',fontsize = 16)
 plt.xticks(rotation=0, fontsize = 14)
 plt.tight_layout()
-plt.savefig("and2.png", dpi=300, bbox_inches='tight')
+plt.savefig("or3.png", dpi=300, bbox_inches='tight')
 
 plt.show()
 
